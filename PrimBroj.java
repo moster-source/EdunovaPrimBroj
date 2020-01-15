25afafbdbd7b46520a5a21ccf837aeeb84352f98
moster-source <51851468+moster-source@users.noreply.github.com>
2020-01-15 16:04:32 +0100

    Initial commit

---
+++ PrimBroj.java
@@ -0,0 +1,43 @@
+import javax.swing.JOptionPane;
+
+public class PrimBroj {
+	
+	//ispis svih prime brojeva do zadanog broja
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//int kojiBroj = 50000;
+		int kojiBroj = Integer.parseInt(JOptionPane.showInputDialog("Unesite maksimalni broj traženja prim broja."));
+		
+		for(int i = 2; i <= kojiBroj; i++) {
+			if(ProvjeriBroj(i)) {
+				System.out.println("Broj " + i + " je prim broj.");
+				
+			}	
+		}
+	}
+
+	
+	//metoda koja provjerava samo jedan broj
+	private static boolean ProvjeriBroj(int broj) {
+		
+		int ostatak;
+		for(int i = 2; i<=(broj/2); i++) {
+			ostatak = broj%i;
+			
+			if (ostatak==0) {
+				return false;
+			}
+		
+			//switch (ostatak){
+			//case 0:
+			//return false;
+			//}
+			
+		}
+		return true;
+		
+	}
+	
+}
